10c440721eeb88a30e727cefdc36e8bc254f09cb
lordacil <rizkynugrahaherliyanto28@gmail.com>
2022-01-25 00:02:38 +0700

    Upload ulang / Migrate ke laptop baru

---
+++ Modul10/app/src/main/java/com/rizkynugraha_19104014/modul10/data/Quote.kt
@@ -0,0 +1,12 @@
+package com.rizkynugraha_19104014.modul10.data
+
+import android.os.Parcelable
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
